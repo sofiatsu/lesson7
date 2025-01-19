4b683d76f3a83b2294963ac1cae0f483c95b6677
sofiatsu <rfjdghjgh@gmail.com>
2025-01-19 13:28:31 +0100

    ДЗ 7.1.

---
+++ lesson7.1.py
@@ -0,0 +1,6 @@
+def say_hi(name, age):
+    return "Hi. My name is %s and I'm %d years old" % (name, age)
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
