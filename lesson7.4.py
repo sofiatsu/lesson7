858565484de49bf93ffed283bd43ff813bf2e001
sofiatsu <rfjdghjgh@gmail.com>
2025-01-21 21:14:11 +0100

    ДЗ 7.4.

--- lesson7.4.py
+++ lesson7.4.py
@@ -2,5 +2,7 @@ def common_elements():
     first = {x for x in range(100) if x % 3 == 0}
     second = {x for x in range(100) if x % 5 == 0}
     return first.intersection(second)
+
+
 assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
 print(common_elements())
